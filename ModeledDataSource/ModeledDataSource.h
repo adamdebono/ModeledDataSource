e9e2ed9c1a13504c124778e25b50562cfe41aa76
Adam Debono <me@adamdebono.com>
2016-04-05 19:19:27 +1000

    Initial Commit

---
+++ ModeledDataSource/ModeledDataSource.h
@@ -0,0 +1,19 @@
+//
+//  ModeledDataSource.h
+//  ModeledDataSource
+//
+//  Created by Adam Debono on 5/04/2016.
+//  Copyright © 2016 Adam Debono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModeledDataSource.
+FOUNDATION_EXPORT double ModeledDataSourceVersionNumber;
+
+//! Project version string for ModeledDataSource.
+FOUNDATION_EXPORT const unsigned char ModeledDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModeledDataSource/PublicHeader.h>
+
+
